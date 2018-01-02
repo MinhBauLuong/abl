54315415aba9b27f53842b472c17cee62729e2c6
Eliot Quon <eliot.quon@nrel.gov>
2018-01-02 11:29:02 -0700

    Added curve fit function for fitting Uref and alpha to a power-law profile

--- meteorology.py
+++ meteorology.py
@@ -27,3 +27,19 @@ def fit_power_law_alpha(z,U,zref=80.0,Uref=8.0):
     R2 = 1.0 - (SSres/SStot)
     return alpha, R2
 
+def fit_power_law_Uref_alpha(z,U,zref=80.0,Umax=25.0):
+    above0 = z > 0
+    logz = np.log(z[above0]) - np.log(zref)
+    logU = np.log(U[above0])
+    func = lambda xdata,alpha,logU: alpha*xdata + logU
+    init_guess = [0.2, np.log(8.0)]  # hard-coded values
+    popt, pcov = curve_fit(func,xdata=logz,ydata=logU,p0=init_guess,
+                           bounds=[(0,0),(1.0,np.log(Umax))])
+    alpha = popt[0]
+    Uref = np.exp(popt[1])
+    resid = U - Uref*(z/zref)**alpha
+    SSres = np.sum(resid**2)
+    SStot = np.sum((U - np.mean(U))**2)
+    R2 = 1.0 - (SSres/SStot)
+    return alpha, Uref, R2
+
