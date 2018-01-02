f138658ef2a0be48de1d94bc8daa831087538bfa
Eliot Quon <eliot.quon@nrel.gov>
2018-01-02 10:50:26 -0700

    Rewrote lambda function as a definition for clarity.
    Added power-law fit function to estimate alpha given a mean wind speed
    profile

--- meteorology.py
+++ meteorology.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+import numpy as np
+from scipy.optimize import curve_fit
 """
 
 Space for meteorological functions, ex:
@@ -9,4 +11,19 @@ Space for meteorological functions, ex:
 
 """
 
-power_law = lambda z, zref, uref, alpha: uref*(z/zref)**alpha
+def power_law(z,zref=80.0,Uref=8.0,alpha=0.2):
+    return Uref*(z/zref)**alpha
+
+def fit_power_law_alpha(z,U,zref=80.0,Uref=8.0):
+    above0 = z > 0
+    logz = np.log(z[above0]) - np.log(zref)
+    logU = np.log(U[above0]) - np.log(Uref)
+    func = lambda xdata,alpha: alpha*xdata
+    popt, pcov = curve_fit(func,xdata=logz,ydata=logU,p0=0.2,bounds=(0,np.inf))
+    alpha = popt[0]
+    resid = U - Uref*(z/zref)**alpha
+    SSres = np.sum(resid**2)
+    SStot = np.sum((U - np.mean(U))**2)
+    R2 = 1.0 - (SSres/SStot)
+    return alpha, R2
+
